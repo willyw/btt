469a192df8608f52e121ee72a151c5fa9bf2a8c0
“Willy <will@rubify.com>
2010-11-25 23:24:37 +0800

    added rails and gitignore

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '94b90b5dfa7d1ade141d4fb4cafa0472f7ed1693dc56a63f83973c0d453ed1f38c24996158a629941d7e23ada4a119fd6335960669d2439b07bb2b47f2d451dc';
